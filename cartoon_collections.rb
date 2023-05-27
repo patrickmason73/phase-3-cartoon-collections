7502f816181fa3cd9dd14dcc2d905e82d3cf3d77
Patrick Mason <patrickmason73@gmail.com>
2023-05-27 17:52:25 -0600

    commit tree 569dc495288141b2921707063a62a32950650687

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
+  dwarves.each.with_index(1) do |dwarf, i|
+    puts "#{i}. #{dwarf}"
+  end
+  dwarves
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(elements)# code an argument here
   # Your code here
+ 
+  elements = elements.map do |name|
+    name.capitalize + "!"
+  end
+  elements
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls = calls.map do |call|
+    call.length
+  end
+  if calls.any? { |call| call > 4 }
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
